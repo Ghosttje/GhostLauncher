cc8876979a6f06a45724c10dab5b72d8aa4c0572
Ghosttje <kevineke@gmail.com>
2015-01-13 12:57:09 +0100

    fix for untracked files

--- GhostLauncher/GhostLauncher.MasterServer/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace GhostLauncher.MasterServer.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
