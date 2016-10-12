2a0717b9fbf7a79bdb823423f98623e28565fac3
Kevin Goos <Kevin Goos>
2016-10-12 22:00:30 +0200

    Trying to get the structure right

--- GhostLauncher/GhostLauncher.Client/Views/MainInstanceWindow.xaml.cs
+++ GhostLauncher/GhostLauncher.Client/Views/MainInstanceWindow.xaml.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace GhostLauncher.Client.Views
+﻿namespace GhostLauncher.Client.Views
 {
     /// <summary>
     /// Interaction logic for MainInstanceWindow.xaml
     /// </summary>
-    public partial class MainInstanceWindow : Window
+    public partial class MainInstanceWindow
     {
         public MainInstanceWindow()
         {
