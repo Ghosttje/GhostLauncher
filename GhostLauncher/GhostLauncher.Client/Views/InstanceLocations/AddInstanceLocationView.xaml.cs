4eee1090630cbf681145af09e7513bf0d6c26163
ghosttje <kevineke@gmail.com>
2016-11-07 22:21:28 +0100

    Rename and starting on a new view

--- GhostLauncher/GhostLauncher.Client/Views/InstanceLocations/AddInstanceLocationView.xaml.cs
+++ GhostLauncher/GhostLauncher.Client/Views/InstanceLocations/AddInstanceLocationView.xaml.cs
@@ -1,9 +1,24 @@
-﻿namespace GhostLauncher.Client.Views.InstanceLocations
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GhostLauncher.Client.Views.InstanceLocations
 {
     /// <summary>
-    /// Interaction logic for AddLocationTypeView.xaml
+    /// Interaction logic for AddInstanceLocationView.xaml
     /// </summary>
-    public partial class AddInstanceLocationView
+    public partial class AddInstanceLocationView : UserControl
     {
         public AddInstanceLocationView()
         {
---
+++ GhostLauncher/GhostLauncher.Client/Views/InstanceLocations/SelectInstanceLocationTypeView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace GhostLauncher.Client.Views.InstanceLocations
+{
+    /// <summary>
+    /// Interaction logic for AddLocationTypeView.xaml
+    /// </summary>
+    public partial class SelectInstanceLocationTypeView
+    {
+        public SelectInstanceLocationTypeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
